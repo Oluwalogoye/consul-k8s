a1846b040795ec70620ee5a9b32e0e5427a8e1f2
Thomas Eckert <teckert@hashicorp.com>
2022-06-10 11:00:23 -0400

    Add proxy read to command map

--- cli/commands.go
+++ cli/commands.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/hashicorp/consul-k8s/cli/cmd/install"
+	"github.com/hashicorp/consul-k8s/cli/cmd/proxy/read"
 	"github.com/hashicorp/consul-k8s/cli/cmd/status"
 	"github.com/hashicorp/consul-k8s/cli/cmd/uninstall"
 	"github.com/hashicorp/consul-k8s/cli/cmd/upgrade"
@@ -46,6 +47,11 @@ func initializeCommands(ctx context.Context, log hclog.Logger) (*common.BaseComm
 			return &cmdversion.Command{
 				BaseCommand: baseCommand,
 				Version:     version.GetHumanVersion(),
+			}, nil
+		},
+		"proxy read": func() (cli.Command, error) {
+			return &read.ReadCommand{
+				BaseCommand: baseCommand,
 			}, nil
 		},
 	}
